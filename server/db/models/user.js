29b4be3784c93c762c251ae62315f7bc3593f0d8
John Asher <johndasher01@gmail.com>
2021-01-11 16:28:33 -0600

    docs: testing pull request

--- server/db/models/user.js
+++ server/db/models/user.js
@@ -11,7 +11,7 @@ const User = db.define('user', {
   password: {
     type: Sequelize.STRING,
     // Making `.password` act like a func hides it when serializing to JSON.
-    // This is a hack to get around Sequelize's lack of a "private" option.
+    // This is a hack to get around Sequelize's lack of a "private" option. This is going to the Moon!
     get() {
       return () => this.getDataValue('password')
     }
