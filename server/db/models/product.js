e9b3ea46294731e75d06e01104d350c56b9eb759
John Asher <johndasher01@gmail.com>
2021-01-12 11:26:53 -0600

    Feature: renamed products db model to singular

--- server/db/models/product.js
+++ server/db/models/product.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize')
 const db = require('../db')
 
-const Products = db.define('product', {
+const Product = db.define('product', {
   name: {
     type: Sequelize.STRING,
     allowNull: false,
@@ -29,4 +29,4 @@ const Products = db.define('product', {
   }
 })
 
-module.exports = Products
+module.exports = Product
