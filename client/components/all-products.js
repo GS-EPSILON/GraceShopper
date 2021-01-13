65f2c48cbd8012748a99bc690b26eaa27bc671c1
John Asher <johndasher01@gmail.com>
2021-01-13 11:43:48 -0600

    Feature: implemented all-products component

--- client/components/all-products.js
+++ client/components/all-products.js
@@ -1,16 +1,42 @@
-// import React from 'react'
+import React from 'react'
+import {connect} from '../../server/api/products'
+import {Link} from 'react-router-dom'
 
-// export class AllProducts extends React.Component {
-//   constructor() {
-//     super()
+export class AllProducts extends React.Component {
+  constructor() {
+    super()
+  }
 
-//   }
+  componentDidMount() {
+    try {
+      this.props.loadAllProducts(this.props.match.params.products)
+    } catch (error) {
+      console.error(error)
+    }
+  }
 
-//   render() {
-//     return (
-//       <div>
-//         <ul></ul>
-//       </div>
-//     )
-//   }
-// }
+  render() {
+    const {products} = this.props
+    return (
+      <div>
+        {products.map(product => (
+          <div key={product.id}>
+            <h2>{product.name}</h2>
+            <img src={product.imageURL} alt={`${product.name}`} />
+            <h2>{product.price}</h2>
+          </div>
+        ))}
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = state => ({
+  products: state.products
+})
+
+const mapDispatchToProps = dispatch => ({
+  loadAllProducts: () => dispatch(fetchProducts())
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(AllProducts)
