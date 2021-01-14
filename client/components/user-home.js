5b6a8b743cf80f1221963ea96f27b371d0c17efa
j-dags <jon.dagdagan@gmail.com>
2021-01-14 15:21:28 -0600

    refactor-nav

--- client/components/user-home.js
+++ client/components/user-home.js
@@ -1,16 +1,46 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import PropTypes from 'prop-types'
-import {connect} from 'react-redux'
+import {connect, useDispatch} from 'react-redux'
+import {Login, Signup} from './auth-form'
+import {logout} from '../store/user'
 
 /**
  * COMPONENT
  */
 export const UserHome = props => {
-  const {email} = props
+  const [status, setStatus] = useState('logged out')
+  const dispatch = useDispatch()
+  const {email, isLoggedIn} = props
+  // if (email) setStatus('logged in')
 
-  return (
+  useEffect(
+    () => {
+      if (email) setStatus('logged in')
+    },
+    [email]
+  )
+
+  console.log(`status: ${status}, email: ${email}`)
+  console.log('props > ', props)
+  console.log('isLoggedIn: >', isLoggedIn)
+  // console.log('props > ', props)
+
+  return isLoggedIn === true ? (
     <div>
       <h3>Welcome, {email}</h3>
+      <a onClick={() => dispatch(logout())}>Log Out</a>
+    </div>
+  ) : status === 'logged out' ? (
+    <div>
+      <h1>Log In</h1>
+      <Login />
+      <a onClick={() => setStatus('sign up')}>Sign Up</a>
+    </div>
+  ) : (
+    <div>
+      <h1>Sign Up</h1>
+      <Signup />
+      <a onClick={() => setStatus('logged out')}>Log In</a>
     </div>
   )
 }
