640d072ffbe407614fc1f6b78bc728005610413b
Kevin Lee <juho.lee96@gmail.com>
2021-01-19 12:14:05 -0600

    updated checkout component

--- client/components/checkout.js
+++ client/components/checkout.js
@@ -1,13 +1,34 @@
 import React from 'react'
 import {connect} from 'react-redux'
 import {ShippingForm} from './shipping-form'
+import {fetchCart} from '../store/cart'
 
-export const Checkout = () => {
-  return (
-    <div className="checkout-container mt-4">
-      <ShippingForm />
-    </div>
-  )
+export class Checkout extends React.Component {
+  componentDidMount() {
+    try {
+      this.props.fetchCart()
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  render() {
+    const {cart} = this.props
+    console.log('CART>>>>>', cart)
+    return (
+      <div className="checkout-container mt-4">
+        <ShippingForm />
+      </div>
+    )
+  }
 }
 
-export default Checkout
+const mapState = state => ({
+  cart: state.cart
+})
+
+const mapDispatch = dispatch => ({
+  fetchCart: () => dispatch(fetchCart())
+})
+
+export default connect(mapState, mapDispatch)(Checkout)
