30ab83c42df4fbcb254ca6cf191f530f78393a1f
Kevin Lee <juho.lee96@gmail.com>
2021-01-20 12:10:27 -0600

    refractored shipping form and checkout

--- client/components/shipping-form.js
+++ client/components/shipping-form.js
@@ -1,64 +1,62 @@
-// import React from 'react'
-// import {Row, Col, Form, FormGroup, Label, Input, Button} from 'reactstrap'
+import React, {useState} from 'react'
 
-
-// export const ShippingForm = props => {
-//   return (
-//     <div className="shipping-form">
-//       <Row>
-//         <Col sm="12" md={{size: 6, offset: 0}} className="text-center">
-//           <h2>Checkout</h2>
-//         </Col>
-//       </Row>
-//       <Row className="mt-4">
-//         <Col sm="12" md={{size: 10, offset: 0}}>
-//           <Form>
-//             <h2>Shipping Adress:</h2>
-//             <FormGroup>
-//               <Label for="email">Email</Label>
-//               <Input name="email" placeholder="Ex: jsnow@email.com" />
-//             </FormGroup>
-//             <FormGroup>
-//               <Label for="fullName">Full Name</Label>
-//               <Input name="fullName" placeholder="Ex: John Snow" />
-//             </FormGroup>
-//             <FormGroup>
-//               <Label for="streetAddress">Street Adress</Label>
-//               <Input name="streetAddress" placeholder="Ex: 123 Snow Moutain" />
-//             </FormGroup>
-//             <FormGroup>
-//               <Label for="city">City</Label>
-//               <Input name="city" placeholder="Ex: Snow City" />
-//             </FormGroup>
-//             <FormGroup>
-//               <Label for="state">State/Province</Label>
-//               <Input name="state" placeholder="Ex: Snow State" />
-//             </FormGroup>
-//             <FormGroup>
-//               <Label for="zipcode">Zipcode</Label>
-//               <Input name="zipcode" placeholder="Ex: 12345" />
-//             </FormGroup>
-
-//             <div />
-//             <h2>Payment Method:</h2>
-//             <FormGroup>
-//               <Label for="cardNumber">Card Number</Label>
-//               <Input name="cardNumber" placeholder="1111 1111 1111 1111" />
-//             </FormGroup>
-//             <FormGroup>
-//               <Label for="expiration">Expiration Date</Label>
-//               <Input name="expiration" placeholder="MM/YY" />
-//             </FormGroup>
-//             <FormGroup>
-//               <Label for="securityCode">Security Code</Label>
-//               <Input name="securityCode" placeholder="CVC" />
-//             </FormGroup>
-//             <p>Billing address same as shipping</p>
-//             <Button>Submit</Button>
-//           </Form>
-//         </Col>
-//       </Row>
-//     </div>
-//   )
-// }
-
+export const ShippingForm = props => {
+  const {handleSubmit, cart} = props
+  const [checkout, setCheckout] = useState(false)
+  const onCheckout = () => {
+    handleSubmit(cart)
+    setCheckout(true)
+  }
+  console.log('CHECKOUT!!!!', checkout)
+  return !checkout ? (
+    <div className="shipping-form">
+      <div className="totalPrice">
+        <h1>Total Price:{cart.totalPrice} </h1>
+      </div>
+      <h2>Checkout</h2>
+      <form>
+        <div className="address">
+          <h2>Shipping Adress:</h2>
+          <label htmlFor="email">Email:</label>
+          <input name="title" type="text" placeholder="Ex: jsnow@email.com" />
+          <label htmlFor="fullName">Full Name:</label>
+          <input name="fullName" type="text" placeholder="Ex: John Snow" />
+          <label htmlFor="streetAddress">Street Address:</label>
+          <input
+            name="streetAddress"
+            type="text"
+            placeholder="Ex: 123 Snow Moutain"
+          />
+          <label htmlFor="city">City:</label>
+          <input name="city" type="text" placeholder="Ex: Snow City" />
+          <label htmlFor="state">State/Province:</label>
+          <input name="state" type="text" placeholder="Ex: Snow State" />
+          <label htmlFor="zipcode">Zipcode:</label>
+          <input name="zipcode" type="text" placeholder="Ex: 123456" />
+        </div>
+        <div className="payment">
+          <h2>Payment Method:</h2>
+          <label htmlFor="cardNumber">Card Number:</label>
+          <input
+            name="cardNumber"
+            type="text"
+            placeholder="1111 1111 1111 1111"
+          />
+          <label htmlFor="expiration">Expiration Date:</label>
+          <input name="expiration" type="text" placeholder="MM/YY" />
+          <label htmlFor="securityCode">Security Code:</label>
+          <input name="securityCode" type="text" placeholder="CVC" />
+        </div>
+        <p>Billing address same as shipping</p>
+        <button type="button" onClick={onCheckout}>
+          Submit
+        </button>
+      </form>
+    </div>
+  ) : (
+    <div>
+      <h1>Thank you.</h1>
+      <h5>YOU'RE ORDER HAS BEEN MADE</h5>
+    </div>
+  )
+}
